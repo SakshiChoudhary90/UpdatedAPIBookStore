510bf95ac7376951bbf2a317d058795c77a6d600
Sakshi Choudhary <Sakshi.Choudhary@NIIT-Tech.com>
2019-04-03 15:32:09 +0530

    Add project files.

---
+++ apiOnlineBookStoreProject/Controllers/CustomerOrderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apiOnlineBookStoreProject.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiOnlineBookStoreProject.Controllers
+{
+    [EnableCors("AllowMyOrigin")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerOrderController : ControllerBase
+    {
+
+    }
+}
